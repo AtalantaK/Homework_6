e4ba23ca12abf55ee9f6d6d8d664ee8802840489
kska0221 <aksinia.ra@gmail.com>
2025-05-24 22:30:25 +0500

    Homework_6

--- Homework_6.java
+++ Homework_6.java
@@ -6,6 +6,7 @@ public class Homework_6 {
     public static void main(String[] args) {
         LocalDate publishDate = LocalDate.now().minusDays(0);
         LocalTime publishTime = LocalTime.now().minusMinutes(60);
+
         LocalDateTime timestamp = LocalDateTime.of(publishDate, publishTime);
 
         //LocalDateTime timestamp = LocalDateTime.of(2015, 01, 21, 20, 50);
